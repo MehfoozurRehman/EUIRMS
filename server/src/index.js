2fac736275ac74ed907e160eb60d6818929126f0
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-03-27 03:18:22 +0500

    fix

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 // api config
 dotenv.config();
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3001;
 // middleware
 
 app.use(express.json());
+app.use(cors());
 
 // db config
 
